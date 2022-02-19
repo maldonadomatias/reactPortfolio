e3d25b6cdaf461bb8f28a56cb06275fb92c4c303
maldonadomatias <molporron@gmail.com>
2022-02-19 20:04:02 -0300

    form

--- src/components/UI/Form.js
+++ src/components/UI/Form.js
@@ -1,18 +1,31 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
-import classes from './Form.module.css'
+import classes from "./Form.module.css";
 
 import emailjs from "@emailjs/browser";
 import { init } from "@emailjs/browser";
 import Button from "./Button";
+// ES6 Modules or TypeScript
+import Swal from 'sweetalert2'
+
 init("user_4DcqQld8FULIfZKDk8vbe");
 
 const Form = () => {
+  const [enteredUsername, setEnteredUsername] = useState("");
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
+    if (enteredUsername.trim().length === 0) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Please enter a valid name and email (non-empty values).',
+        footer: '<a href="">Why do I have this issue?</a>'
+      })
+      return;
+    }
     emailjs
       .sendForm(
         "service_0yal825",
@@ -28,17 +41,31 @@ const Form = () => {
           console.log(error.text);
         }
       );
+    setEnteredUsername("");
   };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
   return (
-    <div className={classes.form}>
+    <div className={classes.card}>
       <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" placeholder="Name" />
-        <label>Email</label>
+        <label htmlFor="user_name">Name</label>
+        <input
+          type="text"
+          name="user_name"
+          placeholder="Name"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
+        <label htmlFor="user_email">Email</label>
         <input type="email" name="user_email" placeholder="Email" />
-        <label>Message</label>
-        <textarea name="message" placeholder="Enter your message"/>
-        <Button type="submit" value="Send">Send Message</Button>
+        <label htmlFor="message">Message</label>
+        <textarea name="message" placeholder="Enter your message" />
+        <Button type="submit" value="Send">
+          Send Message
+        </Button>
       </form>
     </div>
   );
