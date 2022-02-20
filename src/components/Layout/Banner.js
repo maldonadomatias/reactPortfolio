b3b953e368384e92e9df35f3963731e389726c19
maldonadomatias <molporron@gmail.com>
2022-02-20 09:50:54 -0300

    some effects

--- src/components/Layout/Banner.js
+++ src/components/Layout/Banner.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Banner.module.css";
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
+import { BsChevronDoubleDown } from "react-icons/bs";
 
 const Banner = () => {
+  const [scrollDownMessage, setScrollDownMessage] = useState(false);
+  const scrollDown = setTimeout(() => {
+    setScrollDownMessage(true);
+  }, 5000);
   return (
     <div className={classes.title} id="back-to-top">
       <div className={classes["title-row"]}>
@@ -10,8 +15,14 @@ const Banner = () => {
           <h2>
             Hey! I'm <span className={classes.name}> Matías Maldonado</span>
           </h2>
-        <h2>Front-End Developer</h2>
+          <h2>Front-End Developer</h2>
         </Fade>
+        {scrollDownMessage && (
+          <Fade bottom>
+            <p className={classes.scroll}>Scroll Down</p>
+            <BsChevronDoubleDown className={classes.chevron}/>
+          </Fade>
+        )}
       </div>
     </div>
   );
